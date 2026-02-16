69ef3b461d06cb74ced9decd6580fc25d9a65a77
Null Reference <mortalis06@gmail.com>
2026-02-15 21:12:36 -0500

    Moving GluedMessage Dictionary to awaitable version in order to stop changes during saving process

---
+++ Classes/StickyVault.cs
@@ -0,0 +1,115 @@
+﻿using ElmerBot.Models;
+using ElmerBot.Repositories;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace ElmerBot.Classes
+{
+    internal class StickyVault(string jsonFolderLocation, ILogging_Repository logger)
+    {
+        ConcurrentDictionary<string, GluedMessage> stickys => _stickys ??= Load();
+        ConcurrentDictionary<string, GluedMessage> _stickys;
+        System.Timers.Timer saveTimer;
+        bool isSaving = false,
+            initialLoad = true,
+            saveNeeded = false;
+        DateTime? lastUpdate = null;
+
+        internal ICollection<string> Keys => stickys.Keys;
+
+        public async Task<(bool success, GluedMessage? sticky)> TryGetValue(string key)
+        {
+            await WaitForUnlocked();
+            bool success = stickys.TryGetValue(key, out var sticky);
+            return (success, sticky);
+        }
+
+        public async Task<bool> TryAdd(string key, GluedMessage message)
+        {
+            await WaitForUnlocked();
+            saveNeeded = true;
+            return stickys.TryAdd(key, message);
+        }
+
+        public async Task<bool> TryUpdate(string key, GluedMessage newSticky, GluedMessage oldSticky)
+        {
+            await WaitForUnlocked();
+            saveNeeded = true;
+            return stickys.TryUpdate(key, newSticky, oldSticky);
+        }
+
+        public async Task<(bool success, GluedMessage? sticky)> Remove(string key)
+        {
+            await WaitForUnlocked();
+            saveNeeded = true;
+            bool succes = stickys.Remove(key, out var sticky);
+            return(succes, sticky);
+        }
+
+        async Task WaitForUnlocked()
+        {
+            while (isSaving) await Task.Delay(100);
+        }
+
+        ConcurrentDictionary<string, GluedMessage> Load()
+        {
+            if (saveTimer is null)
+            {
+                saveTimer = new System.Timers.Timer(5000);
+                saveTimer.AutoReset = true;
+                saveTimer.Elapsed += async (s, e) =>
+                {
+                    if (saveNeeded)
+                        await Save();
+                };
+                saveTimer.Start();
+            }
+
+            if (Directory.Exists(jsonFolderLocation))
+                if (File.Exists(jsonFolderLocation + "list.json"))
+                    if (JsonSerializer.Deserialize<List<GluedMessage>>(File.ReadAllText(jsonFolderLocation + "list.json")) is List<GluedMessage> m)
+                    {
+                        if (initialLoad)
+                            m.ForEach(m => m.isWatching = false);
+
+                        initialLoad = false;
+
+                        return new ConcurrentDictionary<string, GluedMessage>(m.ToDictionary(k => $"{k.Server_ID}_{k.Channel_ID}", v => v));
+                    }
+            return [];
+        }
+
+        public async Task Save()
+        {
+            isSaving = true;
+
+            try
+            {
+                if (!Directory.Exists(jsonFolderLocation))
+                    Directory.CreateDirectory(jsonFolderLocation);
+                await File.WriteAllTextAsync(jsonFolderLocation + "list.json", JsonSerializer.Serialize(stickys.Select(m => m.Value).ToList()));
+
+                if ((DateTime.Now - (lastUpdate ?? DateTime.Now)).TotalMinutes > 2)
+                {
+                    _stickys = null!;
+                    lastUpdate = null;
+                    await logger.LogBasic("Memory Release", "Released stickys from memory, as it has been over 2 minutes since the last save.");
+                }
+                else
+                {
+                    lastUpdate = DateTime.Now;
+                }
+            }
+            catch (IOException) { }
+            catch (Exception ex)
+            {
+                await logger.LogError($"Error saving stickys", Exception: ex);
+            }
+
+            isSaving = false;
+        }
+    }
+}
--- Repositories/Glue_Repository.cs
+++ Repositories/Glue_Repository.cs
@@ -2,10 +2,12 @@ using DSharpPlus;
 using DSharpPlus.Commands.Processors.SlashCommands;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using ElmerBot.Classes;
 using ElmerBot.Models;
 using Microsoft.Extensions.Options;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Security.Cryptography;
 using System.Text.Json;
 
@@ -18,8 +20,7 @@ namespace ElmerBot.Repositories
         Task ViewStickys(SlashCommandContext ctx);
         Task RemoveServer(ulong serverID);
 
-        void Save();
-        ConcurrentDictionary<string, GluedMessage> GetMessages();
+        StickyVault GetMessages();
 
         Task Process_MessageCreated(DiscordClient c, DSharpPlus.EventArgs.MessageCreatedEventArgs e);
         Task Process_GuildAvailable(DiscordClient c, DiscordGuild guild);
@@ -31,70 +32,9 @@ namespace ElmerBot.Repositories
     {
         Settings settings => _config.Value;
 
-        string settingsFolder = AppDomain.CurrentDomain.BaseDirectory + "/Settings/";
-        internal ConcurrentDictionary<string, GluedMessage> msgs => _msgs ??= Load();
-        ConcurrentDictionary<string, GluedMessage> _msgs;
-        DateTime? lastSavedTime;
-        System.Timers.Timer saveTimer;
-        bool needSave = false;
+        StickyVault msgs = new(AppDomain.CurrentDomain.BaseDirectory + "/Settings/", logger);
 
-        #region JSON File Methods
-
-        internal ConcurrentDictionary<string, GluedMessage> Load()
-        {
-            if (saveTimer is null)
-            {
-                saveTimer = new System.Timers.Timer(5000);
-                saveTimer.AutoReset = true;
-                saveTimer.Elapsed += async (s, e) =>
-                {
-                    if (needSave)
-                        try
-                        {
-                            if (!Directory.Exists(settingsFolder))
-                                Directory.CreateDirectory(settingsFolder);
-                            await File.WriteAllTextAsync(settingsFolder + "list.json", JsonSerializer.Serialize(msgs.Select(m => m.Value).ToList()));
-
-                            needSave = false;
-
-                            if ((DateTime.Now - (lastSavedTime ?? DateTime.Now)).TotalMinutes > 2)
-                            {
-                                _msgs = null!;
-                                lastSavedTime = null;
-                                await logger.LogBasic("Memory Release", "Released stickys from memory, as it has been over 2 minutes since the last save.");
-                            }
-                            else
-                            {
-                                lastSavedTime = DateTime.Now;
-                            }
-                        }
-                        catch (IOException) { }
-                        catch (Exception ex)
-                        {
-                            await logger.LogError($"Error saving stickys", Exception: ex);
-                        }
-                };
-                saveTimer.Start();
-            }
-
-            if (Directory.Exists(settingsFolder))
-                if (File.Exists(settingsFolder + "list.json"))
-                    if (JsonSerializer.Deserialize<List<GluedMessage>>(File.ReadAllText(settingsFolder + "list.json")) is List<GluedMessage> m)
-                    {
-                        if(lastSavedTime is null)
-                            m.ForEach(m => m.isWatching = false);
-                        return new ConcurrentDictionary<string, GluedMessage>(m.ToDictionary(k => $"{k.Server_ID}_{k.Channel_ID}", v => v));
-                    }
-
-            return [];
-        }
-
-        public void Save() => needSave = true;
-
-
-        #endregion
-
-        public ConcurrentDictionary<string, GluedMessage> GetMessages() => msgs;
+        public StickyVault GetMessages() => msgs;
 
         public async Task GlueMessage(SlashCommandContext ctx, string message, ulong channelId)
         {
@@ -128,11 +68,10 @@ namespace ElmerBot.Repositories
                     await ctx.RespondAsync(new DiscordInteractionResponseBuilder().WithContent($"Unable to use the channel {chnl.Mention}. Missing permissions: {String.Join(" ,", missingPerms)}").AsEphemeral());
 
                 }
-                else if (msgs.TryGetValue($"{ctx.Guild!.Id}_{channelId}", out var msg))
+                else if (await msgs.TryGetValue($"{ctx.Guild!.Id}_{channelId}") is (bool success, GluedMessage msg) && success)
                 {
-                    if (msgs.TryUpdate($"{ctx.Guild!.Id}_{channelId}", new GluedMessage(msg) { Message = message }, msg))
+                    if (await msgs.TryUpdate($"{ctx.Guild!.Id}_{channelId}", new GluedMessage(msg) { Message = message }, msg))
                     {
-                        needSave = true;
                         await Task.WhenAll(
                             ctx.RespondAsync(new DiscordInteractionResponseBuilder().WithContent("The message content has been updated.").AsEphemeral()).AsTask(),
                             Process_Sticky(ctx.Client, ctx.Guild!, chnl)
@@ -146,14 +85,12 @@ namespace ElmerBot.Repositories
                 }
                 else
                 {
-                    msgs.TryAdd($"{ctx.Guild!.Id}_{channelId}", new()
+                    await msgs.TryAdd($"{ctx.Guild!.Id}_{channelId}", new()
                     {
                         Server_ID = ctx.Guild!.Id,
                         Channel_ID = channelId,
                         Message = message
                     });
-
-                    needSave = true;
 
                     await Task.WhenAll(
                         ctx.RespondAsync(new DiscordInteractionResponseBuilder().WithContent("The message is now glued.").AsEphemeral()).AsTask(),
@@ -174,10 +111,8 @@ namespace ElmerBot.Repositories
         {
             try
             {
-                if (msgs.Remove($"{ctx.Guild!.Id}_{channelId}", out var msg))
+                if (await msgs.Remove($"{ctx.Guild!.Id}_{channelId}") is (bool success, GluedMessage msg) && success)
                 {
-                    needSave = true;
-
                     try
                     {
                         if (msg.Message_ID > 0)
@@ -216,7 +151,7 @@ namespace ElmerBot.Repositories
                 await ctx.RespondAsync(new DiscordInteractionResponseBuilder().WithContent("Generating list. Please wait..."));
                 List<string> stickyMsgs = [];
                 foreach (var key in keys)
-                    if(msgs.TryGetValue(key, out var msg))
+                    if(await msgs.TryGetValue(key) is (bool succes, GluedMessage msg) && succes)
                     {
                         string sticky = $@"**Channel**: <#{msg.Channel_ID}> (ID: {msg.Channel_ID}
 **Message**: {msg.Message}";
@@ -255,12 +190,8 @@ namespace ElmerBot.Repositories
                 invalidServerKeys = [.. invalidServerKeys.Where(k => !k.StartsWith(serverID.ToString()))];
 
             if (invalidServerKeys.Count != 0)
-            {
                 foreach (var key in invalidServerKeys)
-                    msgs.Remove(key, out _);
-
-                needSave = true;
-            }
+                    await msgs.Remove(key);
         }
 
         #region Processing Methods
@@ -277,10 +208,9 @@ namespace ElmerBot.Repositories
             {
                 List<string> servers = [..invalidServerKeys.Select(k => k.Split("_")[0]).Distinct()];
                 _ = logger.LogBasic("Auto Clean", $"Automatically cleaning invalid servers ({servers.Count}), stickys ({invalidServerKeys.Count}).\r\n- " + String.Join("\r\n- ", servers));
-                foreach (var key in invalidServerKeys)
-                    msgs.Remove(key, out _);
 
-                needSave = true;
+                foreach (var key in invalidServerKeys)
+                    await msgs.Remove(key);
             }
         }
 
@@ -293,7 +223,7 @@ namespace ElmerBot.Repositories
                 List<Task> tasks = [];
 
                 foreach (var key in msgs.Keys.Where(k => k.StartsWith($"{guild.Id}_")))
-                    if (msgs.TryGetValue(key, out var msg))
+                    if (await msgs.TryGetValue(key) is (bool succes, GluedMessage msg) && succes)
                     {
                         DiscordChannel? chnl = null;
                         try { chnl = await guild.GetChannelAsync(msg.Channel_ID); }
@@ -301,24 +231,21 @@ namespace ElmerBot.Repositories
                         {
                             if (msg.Channel_Errors < 10)
                             {
-                                if (msgs.TryGetValue(key, out var gluedMessage))
-                                    msgs.TryUpdate(key, new GluedMessage(gluedMessage) { Channel_Errors = msg.Channel_Errors + 1 }, gluedMessage);
+                                if (await msgs.TryGetValue(key) is (bool success, GluedMessage gluedMessage) && succes)
+                                    await msgs.TryUpdate(key, new GluedMessage(gluedMessage) { Channel_Errors = msg.Channel_Errors + 1 }, gluedMessage);
                                 _ = logger.LogError($"Failed to access channel for sticky.\r\n**Server**: {guild.Name} ({guild.Id})\r\n**Channel ID**: {msg.Channel_ID}");
                             }
                             else
                             {
-                                msgs.Remove($"{msg.Server_ID}_{msg.Channel_ID}", out _);
+                                await msgs.Remove($"{msg.Server_ID}_{msg.Channel_ID}");
                                 _ = logger.LogError($"Failed to access channel for sticky after multiple attempts. Removing sticky.\r\n**Server**: {guild.Name} ({guild.Id})\r\n**Channel ID**: {msg.Channel_ID}");
                             }
-
-                            needSave = true;
                         }
 
                         if (chnl is not null)
                         {
                             DiscordMessage? lastMsg = null;
                             try { lastMsg = await chnl.GetMessagesAsync(1).FirstAsync(); }
-
                             catch { }
 
                             tasks.Add(Process_Sticky(c, guild, chnl, lastMsg));
@@ -331,11 +258,8 @@ namespace ElmerBot.Repositories
 
         public async Task Process_ChannelDeleted(DiscordClient c, ChannelDeletedEventArgs e)
         {
-            if (msgs.Remove($"{e.Guild!.Id}_{e.Channel.Id}", out _))
-            {
-                needSave = true;
+            if (await msgs.Remove($"{e.Guild!.Id}_{e.Channel.Id}") is (bool success, _) && success)
                 await logger.LogBasic("Auto Clean", $"Sticky automatically deleted as channel was deleted.\r\n**Server**: {e.Guild.Name} ({e.Guild.Id})\r\n**Channel**: \\#{e.Channel.Name} ({e.Channel.Id})");
-            }
         }
 
         public async Task Process_MessageCreated(DiscordClient c, MessageCreatedEventArgs e) => await Process_Sticky(c, e.Guild, e.Channel, e.Message);
@@ -347,16 +271,15 @@ namespace ElmerBot.Repositories
             {
                 string msgKey = $"{guild.Id}_{channel.Id}";
                 if (settings.EnabledServers.Contains(guild.Id) || settings.Admin.ServerID == guild.Id)
-                    if (msgs.TryGetValue(msgKey, out var message))
+                    if (await msgs.TryGetValue(msgKey) is (bool success, GluedMessage message) && success)
                         if ((m is null || m.Id != message.Message_ID) && !message.isWatching)
                         {
                             _ = logger.LogBasic("Processing Sticky", $"**Server**: {guild.Name} ({guild.Id}) -> \\#{channel.Name} ({channel.Id})");
 
-                            msgs.TryUpdate(msgKey, new GluedMessage(message) { isWatching = true }, message);
-                            needSave = true;
+                            await msgs.TryUpdate(msgKey, new GluedMessage(message) { isWatching = true }, message);
                             await Task.Delay(5 * 1000);
 
-                            if (msgs.TryGetValue(msgKey, out var msg))
+                            if (await msgs.TryGetValue(msgKey) is (bool getSuccess, GluedMessage msg) && getSuccess)
                                 try
                                 {
                                     try
@@ -401,13 +324,13 @@ namespace ElmerBot.Repositories
                                         {
                                             if (msg.Webhook_Errors < 10)
                                             {
-                                                if (msgs.TryGetValue(msgKey, out var hookErrorMsg))
-                                                    msgs.TryUpdate(msgKey, new GluedMessage(hookErrorMsg) { Webhook_Errors = msg.Webhook_Errors + 1 }, hookErrorMsg);
+                                                if (await msgs.TryGetValue(msgKey) is (bool getHookSuccess, GluedMessage hookErrorMsg) && getHookSuccess)
+                                                    await msgs.TryUpdate(msgKey, new GluedMessage(hookErrorMsg) { Webhook_Errors = msg.Webhook_Errors + 1 }, hookErrorMsg);
                                                 _ = logger.LogError($"Failed to generate a webhook for sticky.\r\n**Server**: {guild.Name} ({guild.Id})\r\n**Channel:** \\#{channel.Name} ({channel.Id})");
                                             }
                                             else
                                             {
-                                                msgs.Remove($"{msg.Server_ID}_{msg.Channel_ID}", out _);
+                                                await msgs.Remove($"{msg.Server_ID}_{msg.Channel_ID}");
                                                 _ = logger.LogError($"Failed to generate a webhook for sticky after multiple attempts. Removing sticky.\r\n**Server**: {guild.Name} ({guild.Id})\r\n**Channel**: \\#{channel.Name} ({channel.Id})");
                                             }
                                         }
@@ -424,8 +347,8 @@ namespace ElmerBot.Repositories
                                             };
                                             DiscordMessage hookMsg = await hook.ExecuteAsync(builder);
 
-                                            if (msgs.TryGetValue(msgKey, out var successfulMessage))
-                                                msgs.TryUpdate(msgKey, new GluedMessage(successfulMessage)
+                                            if (await msgs.TryGetValue(msgKey) is (bool submittedSucces, GluedMessage successfulMessage) && submittedSucces)
+                                                await msgs.TryUpdate(msgKey, new GluedMessage(successfulMessage)
                                                     { 
                                                         Message_ID = hookMsg.Id,
                                                         Channel_Errors = 0,
@@ -441,9 +364,8 @@ namespace ElmerBot.Repositories
                                             _ = logger.LogError($"Error during webhook submission.\r\n**Server**: {guild.Name} ({guild.Id})\r\n**Channel:** \\#{channel.Name} ({channel.Id})", guild, ex);
                                         }
 
-                                    if (msgs.TryGetValue(msgKey, out var gluedMessage))
-                                        msgs.TryUpdate(msgKey, new GluedMessage(gluedMessage) { isWatching = false }, gluedMessage);
-                                    needSave = true;
+                                    if (await msgs.TryGetValue(msgKey) is (bool watchingSucces, GluedMessage gluedMessage) && watchingSucces)
+                                        await msgs.TryUpdate(msgKey, new GluedMessage(gluedMessage) { isWatching = false }, gluedMessage);
                                 }
                                 catch (Exception ex)
                                 {
